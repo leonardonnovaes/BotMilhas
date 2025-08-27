175b3021a5f92648dc62a7db746ce6932e6d38ec
Leonardo <novaesleo2005@gmail.com>
2025-08-27 16:19:43 -0300

    Criando barramentos por falta de interação

--- api/enviar.php
+++ api/enviar.php
@@ -2,6 +2,9 @@
 require_once('../conn.php');
 $usuario_get = $_GET['usuario'];
 
+error_reporting(0);
+ini_set("display_errors", 0);
+
 $busca_cliente = "SELECT * FROM envios WHERE usuario = '$usuario_get' AND status = '1' ORDER BY id DESC";
 $cliente = mysqli_query($conn, $busca_cliente);
 
@@ -21,4 +24,27 @@ if(  $telefone == True){
 
 }
 
+?>
+
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+$now = time();
+$data_hora = date('Y-m-d H:i:s', $now);
+$data_hora_limite = date('Y-m-d H:i:s',strtotime('-1 minutes', $now));
+
+$busca_pedidos = "SELECT * FROM cliente WHERE data_hora < '$data_hora_limite' AND situacao != 'analise'";
+$resultado_pedido = mysqli_query($conn, $busca_pedidos);
+while($dados_pedidos = mysqli_fetch_array($resultado_pedido)){
+$id_cliente = $dados_pedidos['id'];
+$nome_pedidos = $dados_pedidos['nome'];
+$telefone_pedidos = $dados_pedidos['telefone'];}
+if($id_cliente == TRUE){
+    $msg = "Por falta de interação seu atendimento foi encerrado!";
+    $sql = "INSERT INTO envios (telefone, msg , status,usuario) VALUES ('$telefone_pedidos','$msg', '1','$usuario_get')";
+    $query = mysqli_query($conn, $sql);
+    if($query){
+        $sql = "DELETE FROM cliente WHERE id='$id_cliente'";
+        $query = mysqli_query($conn, $sql);
+    }
+}
 ?>
